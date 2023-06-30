b99c807581bd578c07a16e4ac7eef2f84fabc156
agungd3v <agungd3v@gmail.com>
2023-06-30 19:07:47 +0700

    update sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -13,7 +13,7 @@ export default function Sidebar({children}: CProps) {
   const router = useRouter();
 
   useEffect(() => {
-    console.log(router.asPath.split("/")[2]);
+    // console.log(router.asPath.split("/")[2]);
   }, [router]);
 
   return (<div className="drawer lg:drawer-open">
@@ -30,44 +30,46 @@ export default function Sidebar({children}: CProps) {
     </div>
     <div className="drawer-side border-r-0 shadow rounded-r-lg overflow-y-visible md:border-r">
       <label htmlFor="sidebar" className="drawer-overlay"></label>
-      <ul className="menu p-4 w-64 h-full bg-white text-base-content">
+      <ul className="menu p-4 w-64 h-full bg-white text-base-content flex-nowrap w-full">
         <div className="py-6 mb-3">
           <span className="text-3xl font-semibold">AAADC</span><sub className="text-primary">v1.0.0</sub>
         </div>
-        {routes.map((rts: any, key: number) => {
-          if (!rts.children) return (
-            <li
-              className={`text-base mb-2 rounded-lg menu-item ${router.asPath === rts.path ? "menu-active" : ""}`}
-              key={key}
-            >
-              <div className="flex items-center gap-4">
-                <div className="aricons">{rts.icon}</div>
-                <Link href={rts.path} className="text-sm w-full">{rts.label}</Link>
-              </div>
-            </li>
-          );
-          if (rts.children) return (
-            <li
-              className={`rounded-lg menu-item-nested text-base dropdown dropdown-right mb-2 ${router.asPath.split("/")[2] === rts.path.split("/")[2] ? "menu-active" : ""}`}
-              key={key}
-            >
-              <label tabIndex={0} className="w-full flex items-center justify-between">
+        <div className="h-full">
+          {routes.map((rts: any, key: number) => {
+            if (!rts.children) return (<div key={key}>
+              <li
+                className={`w-full text-base mb-2 rounded-lg menu-item ${router.asPath === rts.path ? "menu-active" : ""}`}
+              >
                 <div className="flex items-center gap-4">
                   <div className="aricons">{rts.icon}</div>
-                  <span className="text-sm">{rts.label}</span>
+                  <Link href={rts.path} className="text-sm w-full">{rts.label}</Link>
                 </div>
-                <div className="aricons"><FiChevronRight size={20} /></div>
-              </label>
-              <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-lg w-52">
-                {rts.children.map((chil: any, cKey: number) => {
-                  return <li key={cKey}>
-                    <Link href={rts.path + chil.path} className={`nested-item ${rts.path + chil.path == router.asPath ? "nested-active" : ""}`}>{chil.label}</Link>
-                  </li>
-                })}
-              </ul>
-            </li>
-          );
-        })}
+              </li>
+            </div>
+            );
+            if (rts.children) return (<div className="" key={key}>
+              <li
+                className={`w-full rounded-lg menu-item-nested text-base dropdown dropdown-right mb-2 ${router.asPath.split("/")[2] === rts.path.split("/")[2] ? "menu-active" : ""}`}
+              >
+                <label tabIndex={0} className="w-full flex items-center justify-between">
+                  <div className="flex items-center gap-4">
+                    <div className="aricons">{rts.icon}</div>
+                    <span className="text-sm">{rts.label}</span>
+                  </div>
+                  <div className="aricons"><FiChevronRight size={20} /></div>
+                </label>
+                <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-lg w-52">
+                  {rts.children.map((chil: any, cKey: number) => {
+                    return <li key={cKey}>
+                      <Link href={rts.path + chil.path} className={`nested-item ${rts.path + chil.path == router.asPath ? "nested-active" : ""}`}>{chil.label}</Link>
+                    </li>
+                  })}
+                </ul>
+              </li>
+            </div>
+            );
+          })}
+        </div>
       </ul>
     </div>
   </div>);
